7ba7b4d0346577ecef7baff824032061d24ec86a
MatheusWDB <matheuswdb1703@gmail.com>
2024-09-23 10:01:42 -0300

    configuração css baseline, material ui theme

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme, responsiveFontSizes } from "@mui/material"
+import { green, purple } from '@mui/material/colors';
+
+let theme = createTheme({
+    palette: {
+        primary: {
+            main: purple[500],
+        },
+        secondary: {
+            main: green[500],
+        },
+    },
+    typography: {
+        fontFamily: "sans-serif",
+    }
+});
+
+theme = responsiveFontSizes(theme)
+
+export default theme
